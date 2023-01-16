aee4134690785bf7f6c40fb236af9a2353412f39
Victor Cruz <motoggb979@gmail.com>
2023-01-16 18:08:03 -0300

    feat: importando os inputs no app para testes e criação das validações iniciais dos campos

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { FieldValues } from "react-hook-form/dist/types";
+import Input, { moneyFormat } from "./components/Input/input";
 
 function App() {
+  const schema = yup.object().shape({
+    money: moneyFormat.required("valor é requerido"),
+    parcelas: yup
+      .number()
+      .required("parcelas é requerido")
+      .max(12, "parcelas máximas são 12")
+      .min(1, "parcelas minimas são 1"),
+    percent: yup.string().required(),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: FieldValues) => console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Input
+          register={register}
+          placeholder="teste"
+          name="money"
+          error={errors?.money?.message}
+          format="money"
+          type="text"
+        />
+        <Input
+          register={register}
+          placeholder="teste"
+          name="parcelas"
+          error={errors?.parcelas?.message}
+          type="text"
+        />
+        <Input
+          register={register}
+          placeholder="teste"
+          name="percent"
+          error={errors?.percent?.message}
+          type="text"
+        />
+        <button type="submit">click me</button>
+      </form>
     </div>
   );
 }
